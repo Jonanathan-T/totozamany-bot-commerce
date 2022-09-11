83d13cfb353325ac2a5182edbcd71075f61f188e
Totozamany Jonathan <53228019+Jonanathan-T@users.noreply.github.com>
2022-09-11 09:07:43 +0300

    First commit

---
+++ core.py
@@ -0,0 +1,31 @@
+import ampalibe
+from ampalibe import Messenger, webserver
+
+chat = Messenger()
+
+# create a get started option to get permission of user.
+# chat.get_started()
+
+
+@webserver.get('/hello')
+def hello():
+    return 'Hello from SMILE Technologie'
+
+
+@ampalibe.command('/')
+def main(sender_id, cmd, **extends):
+    '''
+    main function where messages received on
+    the facebook page come in.
+
+    @param sender_id String: 
+        sender facebook id
+    @param cmd String: 
+        message content
+    @param extends Dict: 
+        contain list of others
+            data sent by facebook (sending time, ...)
+            data sent by your payload if not set in parameter
+    '''
+
+    chat.send_message(sender_id, "Hello, Ampalibe")
